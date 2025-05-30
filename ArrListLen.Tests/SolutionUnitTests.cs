88087ad7b1e8549bed3bfecb38e7d4a5d81389fa
Sabri Khodja <sabri.khodja@gmail.com>
2025-05-30 02:55:43 +0200

    Implement unit test for given case

--- ArrListLen.Tests/SolutionUnitTests.cs
+++ ArrListLen.Tests/SolutionUnitTests.cs
@@ -2,6 +2,14 @@
 {
     public class SolutionUnitTests
     {
-        
+        [Fact]
+        public void GivenArrayRepresentingLinkedList_WhenSolutionIsCalled_ThenReturnsCorrectLength()
+        {
+            var solution = new ArrListLen.Solution();
+            int[] A = { 1, 4, -1, 3, 2 };
+            int expected = 4;
+            int actual = solution.solution(A);
+            Assert.Equal(expected, actual);
+        }
     }
 }
